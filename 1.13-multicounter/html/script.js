288d06de1455a4a118618801661a7e48699ceaee
chaudhuree <chaudhuree00@gmail.com>
2023-02-24 22:47:12 +0600

    assignment-01 multicounter done

---
+++ 1.13-multicounter/html/script.js
@@ -0,0 +1,145 @@
+//multi counter with redux and vanilla js
+// select dom element
+const Container = document.getElementById("container");
+const AddAnotherMatch = document.getElementById("addAnotherMatch");
+const Reset = document.getElementById("reset");
+
+// action identifiers
+const INCREMENT = "increment";
+const DECREMENT = "decrement";
+const ADDANOTHERMATCH = "addAnotherMatch";
+const RESET = "reset";
+
+
+// action creators
+const increment = (id, value) => {
+  return {
+    type: INCREMENT,
+    payload: { id, value },
+  };
+};
+
+const decrement = (id, value) => {
+  return {
+    type: DECREMENT,
+    payload: { id, value },
+  };
+};
+const addAnotherMatch = () => {
+  return {
+    type: ADDANOTHERMATCH,
+  };
+};
+const reset = () => {
+  return {
+    type: RESET,
+  };
+};
+
+
+// initial state
+const initialState = [{
+  id: 1,
+  value: 0,
+}];
+
+// create reducer function
+function counterReducer(state = initialState, action) {
+  switch (action.type) {
+    case INCREMENT:
+      return state.map(item => {
+        if (item.id === action.payload.id) {
+          return { ...item, value: item.value + action.payload.value }
+        }
+        return { ...item };
+      })
+    case DECREMENT:
+      return state.map(item => {
+        if (item.id === action.payload.id) {
+          return { ...item, value: item.value > action.payload.value ? item.value - action.payload.value : 0 }
+
+        }
+        return { ...item };
+      })
+    case ADDANOTHERMATCH:
+      return [...state, { id: state.length + 1, value: 0 }]
+    case RESET:
+      return state.map(item => ({ ...item, value: 0 }))
+    default:
+      return state;
+  }
+}
+// create store
+const store = Redux.createStore(counterReducer);
+
+// render function
+function render() {
+  const state = store.getState();
+  console.log(state);
+  let html = "";
+  state.map((item) => {
+    html += `
+    <div class="match" id="cdcontainer">
+        <div class="wrapper">
+            <button class="lws-delete">
+                <img src="./image/delete.svg" alt="" />
+            </button>
+            <h3 class="lws-matchName">Match ${item.id}</h3>
+        </div>
+        <div class="inc-dec">
+            <form class="incrementForm" id="form${item.id}" data-id=${item.id} >
+                <h4>Increment</h4>
+                <input
+                    type="number"
+                    name="increment"
+                    class="lws-increment"
+                    data-id=${item.id}
+                    onblur="HandleFunction(event, this.value)" 
+                />
+                
+            </form>
+            <form class="decrementForm">
+                <h4>Decrement</h4>
+                <input
+                    data-id=${item.id}
+                    type="number"
+                    name="decrement"
+                    class="lws-decrement"
+                    onblur="HandleFunction(event, this.value)" 
+                />
+            </form>
+        </div>
+        <div class="numbers">
+            <h2 class="lws-singleResult">${item.value}</h2>
+        </div>
+    </div>`
+    Container.innerHTML = html;
+  });
+}
+render()
+// subscribe to store
+store.subscribe(render);
+
+// dispatch action
+AddAnotherMatch.addEventListener("click", () => {
+  store.dispatch(addAnotherMatch());
+});
+Reset.addEventListener("click", () => {
+  store.dispatch(reset());
+});
+
+// action dispatching function
+function HandleFunction(event, value) {
+  event.preventDefault();
+  // console.log(value);
+  // console.log(event.target.dataset.id);
+  // console.log(event.target.name);
+if(event.target.name === "increment"){
+  store.dispatch(increment(+event.target.dataset.id, +value));
+}
+if(event.target.name === "decrement"){
+  store.dispatch(decrement(+event.target.dataset.id, +value));
+}
+  
+}
+
