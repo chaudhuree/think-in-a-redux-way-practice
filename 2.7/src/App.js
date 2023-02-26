ae2775fb6351dc95a3cda0c7a6fa51cc0205ff53
chaudhuree <chaudhuree00@gmail.com>
2023-02-26 23:37:53 +0600

    2.7 done

---
+++ 2.7/src/App.js
@@ -0,0 +1,21 @@
+import { Provider } from "react-redux";
+import DynamicHooksCounter from "./components/DynamicHooksCounter";
+import HooksCounter from "./components/HooksCounter";
+import store from "./redux/store";
+
+export default function App() {
+    return (
+        <Provider store={store}>
+            <div className="w-screen h-screen p-10 bg-gray-100 text-slate-700">
+                <h1 className="max-w-md mx-auto text-center text-2xl font-bold">
+                    Simple Counter Application
+                </h1>
+
+                <div className="max-w-md mx-auto mt-10 space-y-5">
+                    <HooksCounter />
+                    <DynamicHooksCounter />
+                </div>
+            </div>
+        </Provider>
+    );
+}
